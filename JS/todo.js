d29b2f866c5cc96e7fbccc9f0828dc2edde5626b
BIDANSPIN <manman040404@gmail.com>
2026-03-09 00:32:18 +0900

    add todo sumit button

--- JS/todo.js
+++ JS/todo.js
@@ -1,4 +1,4 @@
-const toDoForm = document.getElementById("todo-form");ㅂㅂ
+const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
